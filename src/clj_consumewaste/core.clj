db7467582fc572c54b5812996d23dbdbcf0c9914
liuchunhua <liuchunhua-2000@163.com>
2016-12-22 09:22:08 +0800

    保存高德数据

--- src/clj_consumewaste/core.clj
+++ src/clj_consumewaste/core.clj
@@ -1,7 +1,7 @@
 (ns clj-consumewaste.core
   (:use
    [clojure.java.jdbc :exclude (resultset-seq)]
-   [clojure.string :only (join)])
+   [clojure.string :only (join split)])
   (:require [clojure.core.reducers :as r]
             [clojure.java.io :as io]
             [taoensso.carmine :as car :refer (wcar)])
@@ -29,6 +29,9 @@
 (def pooled-db (delay (db_pool db)))
 
 (defn db-conn [] @pooled-db)
+
+(def server1-conn {:pool {} :spec {:host "10.180.29.70" :port 6379}}) ; See `wcar` docstring for opts
+(defmacro wcar* [& body] `(car/wcar server1-conn ~@body))
 
 (defn carno_extrace
   "获取消费记录中所有的卡号"
@@ -120,12 +123,71 @@
        (r/fold merge-counts)))
 
 (defn save_freq
+  "保存省界收费站及频率"
   [f_name]
   (let [freq (parse_all_shengjie_sfz)]
     (with-open [wr (io/writer f_name)]
       (doseq [[k v] (vec freq)]
         (.write wr (str k ":" v "\n"))))))
 
+(defn gaode
+  [key value]
+  (let [col (split key #":")
+        name-map (zipmap [:station :attr] (split (col 1) #"[()]"))
+        lng (Float/parseFloat (col 2))
+        lat (Float/parseFloat (col 3))]
+    (merge value name-map {:lng lng,:lat lat})))
+
+(defn redis-keyscan
+  "Redis 扫描key"
+  [pattern limit]
+  (loop [cursor 0
+         result (transient [])]
+    (let [[c coll] (wcar* (car/scan cursor :match pattern :count limit))]
+      (if (= "0" c)
+        (apply concat (persistent! (conj! result coll)))
+        (recur c (conj! result coll))))))
+
+(defn create-gaode-table
+  []
+  (db-do-commands (db-conn)
+                  ["drop table gaode_station"
+                   (create-table-ddl :gaode_station
+                                     [[:id "VARCHAR2(16)"]
+                                      [:station "VARCHAR2(64)"]
+                                      [:attr "VARCHAR2(64)"]
+                                      [:lng "NUMBER(16,6)"]
+                                      [:lat "NUMBER(16,6)"]
+                                      [:province "VARCHAR2(32)"]
+                                      [:city "VARCHAR2(64)"]
+                                      [:ad "VARCHAR2(64)"]
+                                      [:address "VARCHAR2(80)"]
+                                      [:pcode "VARCHAR2(8)"]
+                                      [:citycode "VARCHAR2(8)"]
+                                      [:adcode "VARCHAR2(8)"]
+                                      [:tag "VARCHAR2(80)"]])
+                   "create index IDX_GAODE_LNGLAT on gaode_station(lng,lat)"
+                   "create index IDX_GAODE_PROVINCE on gaode_station(pcode,citycode)"]))
+(defn insert-gaode-data
+  ([] 0)
+  ([i coll] (+ i (count (insert-multi! (db-conn) :gaode_station coll)))))
+
+(defn count_num
+  ([] 0)
+  ([& m] (apply + m)))
+
+(defn save-gaode
+  []
+  (->> (wcar* (car/keys "GAODE:*"))
+       (partition-all 256)
+       (mapcat (fn [keys] (map (fn [x y] [x y]) keys (wcar* (mapv car/hgetall* keys))))) ;[([k1,v1] [k2,v2],...),...]
+       (partition-all 20)
+       vec
+       (r/map (fn [coll] (prn (count coll))(map #(apply gaode %) coll)))
+       (r/fold + insert-gaode-data)
+       ))
+
 (defn -main
   [& args]
-  (save_freq "freq.txt"))
+  (create-gaode-table)
+  (save-gaode))
